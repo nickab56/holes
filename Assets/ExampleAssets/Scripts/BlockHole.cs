0eeb636971f5293c65da91506ee20ab3c427f183
Jake Buhite <jakebuhite@gmail.com>
2023-04-16 21:27:47 -0400

    Added additional juicing

--- Assets/ExampleAssets/Scripts/BlockHole.cs
+++ Assets/ExampleAssets/Scripts/BlockHole.cs
@@ -7,10 +7,12 @@ public class BlockHole : MonoBehaviour
 
     public SceneManager sceneManager;
 
+    private AudioSource plankSFX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        plankSFX = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,10 @@ public class BlockHole : MonoBehaviour
 
     private void OnTriggerEnter(Collider collider) {
         if (collider.gameObject.CompareTag("Hole")) {
+            if (!plankSFX.isPlaying)
+            {
+                plankSFX.PlayOneShot(plankSFX.clip, 1f);
+            }
             Vector3 holePos = collider.gameObject.transform.position;
             Destroy(collider.gameObject);
             this.transform.position = holePos;
